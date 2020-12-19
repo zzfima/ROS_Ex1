32a168c70ffbc1d1742b57afd000b7b62ed96e34
zzfima <zzfima@gmail.com>
2020-12-19 16:04:43 +0300

    Update robot_mover.py

--- robot_mover.py
+++ robot_mover.py
@@ -27,7 +27,7 @@ class RobotMoving:
         (roll, pitch, yaw) = euler_from_quaternion (orientation_list) 
         self._current_degree = self.__convert_to_normal_degrees(yaw)
         self._curent_poition = msg.pose.pose.position
-        print(self._curent_poition)
+        # print(self._curent_poition)
 
     def __convert_to_normal_degrees(self, rad):
         """
@@ -50,23 +50,26 @@ class RobotMoving:
         deg = self.__radians_to_degrees(rad)    
         if deg < 0: 
             deg += 360
-        return deg
+        return 360 - deg
         
     def __radians_to_degrees(self, rad):
-        return rad * 180 / math.pi
+        return rad * 180.0 / math.pi
+
+    def __degrees_to_radians(self, deg):
+        return deg *  math.pi / 180.0
 
     def rotation_in_place(self, desired_degree):
         """
         Rotate robot by specific angle
         """
         if desired_degree > self._current_degree:
-            z_num = 0.3
-        else:
             z_num = -0.3
+        else:
+            z_num = 0.3
         while not rospy.is_shutdown():
             rounded_current_degree = int((round(self._current_degree)))
             if desired_degree == rounded_current_degree:
-                print('Finish')
+                print('Finish rotate')
                 break
             self._velocity_command.angular.z = z_num
             self._velocity_publisher.publish(self._velocity_command)
@@ -74,10 +77,32 @@ class RobotMoving:
 
     def move_forvard(self, distance):
         initial_position = self._curent_poition
+        rad = self.__degrees_to_radians(self._current_degree)
+
+        x_distination = initial_position.x - distance * math.cos(rad)
+        y_distination = distance * math.sin(rad) + initial_position.y
+
+        x_distination = round(x_distination, 2)
+        y_distination = round(y_distination, 2)
+
+        print('current position:', initial_position)
+        print('distination: ', x_distination, y_distination)
+        print('current angle: ', self._current_degree)
+        cnt = 0
         while not rospy.is_shutdown():
-            while abs(self._curent_poition.x - initial_position.x) < distance:
-                self._velocity_command.linear.x = -0.2
-                self._velocity_publisher.publish(self._velocity_command)
-                self._rate.sleep()
-            break
+            curent_poition_x = round(self._curent_poition.x, 2)
+            curent_poition_y = round(self._curent_poition.y, 2)
+
+            cnt +=1
+            if cnt == 20:
+                print('now i am at: ', curent_poition_x, curent_poition_y)
+                cnt = 0
+
+            if x_distination == curent_poition_x and y_distination == curent_poition_y:
+                print('Finish forward')
+                break 
+            self._velocity_command.linear.x = -0.2
+            self._velocity_command.linear.y = -0.2
+            self._velocity_publisher.publish(self._velocity_command)
+            self._rate.sleep()
 
